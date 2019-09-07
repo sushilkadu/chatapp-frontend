d96ed5883fe3bf822f68f592de24bc1d5347beea
Sushil Kadu <sushil.kadu@orangebitsindia.com>
2019-09-07 15:40:48 +0530

    Added auth guard

---
+++ src/app/home/auth.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core"
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router
+} from "@angular/router"
+import { Observable } from "rxjs"
+import { TokenService } from "../auth/token-service/token.service"
+
+@Injectable({
+  providedIn: "root"
+})
+export class AuthGuard implements CanActivate {
+  constructor(private tokenService: TokenService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    // check if token is available
+    if (this.tokenService.isLoggedIn()) return true
+
+    this.router.navigate(["auth"])
+    return false
+  }
+}
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core"
 import { Routes, RouterModule } from "@angular/router"
 import { HomeScreenComponent } from "./home-screen/home-screen.component"
+import { AuthGuard } from "./auth.guard"
 
 const routes: Routes = [
   {
     path: "",
-    component: HomeScreenComponent
+    component: HomeScreenComponent,
+    canActivate: [AuthGuard]
   }
 ]
 
